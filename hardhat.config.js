e9cb70da51b0b3297cced1000662964c492f4526
Carol48-mwangi <mwangicaroline517@gmail.com>
2024-07-24 21:10:45 +0100

    Add files via upload

---
+++ hardhat.config.js
@@ -0,0 +1,23 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+require("dotenv").config();
+
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.17",
+  defaultNetwork: "sepolia",
+  networks: {
+    hardhat: {
+    },
+    sepolia: {
+      url: process.env.SEPOLIA_ALCHEMY_RPC_URL,
+      // @ts-ignore
+      accounts: [process.env.PRIVATEKEY],
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+};
